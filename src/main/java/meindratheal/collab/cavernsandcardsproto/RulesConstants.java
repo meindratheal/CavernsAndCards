54a8ec57bbe25fb873334aeef12e0d8da9fc90a8
meindratheal <graftontt@gmail.com>
2016-12-11 03:39:35 +0000

    Created a class to house "magic" constants that come from the rules of the game, such as minimum deck size and maximum hand size,

---
+++ src/main/java/meindratheal/collab/cavernsandcardsproto/RulesConstants.java
@@ -0,0 +1,37 @@
+package meindratheal.collab.cavernsandcardsproto;
+
+/**
+ * Contains constants defined by the rules.
+ * @author Meindratheal
+ */
+public final class RulesConstants
+{
+	/**
+	 * Suppress default constructor.
+	 */
+	private RulesConstants()
+	{
+		throw new AssertionError();
+	}
+
+	/**
+	 * The minimum number of cards required in a deck when starting a game.
+	 * @return The minimum deck size
+	 */
+	public static int minDeckSize()
+	{
+		return 10;
+	}
+
+	/**
+	 * The maximum number of cards allowed in a player's hand at the end of
+	 * their turn. This limit may be exceeded at any other point, but if a
+	 * player's hand has more cards than this value at the end of their turn
+	 * then they must discard cards until they fall below this limit.
+	 * @return The maximum hand size allowed at the end of the turn.
+	 */
+	public static int maxHandSize()
+	{
+		return 4;
+	}
+}
